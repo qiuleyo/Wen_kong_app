a7ff33ae27a9f912f54c39bea825f4dc08819e70
qiule <3042627767@qq.com>
2022-05-13 18:26:41 +0000

    update Wen_kong.sh.

--- Wen_kong.sh
+++ Wen_kong.sh
@@ -7,7 +7,7 @@ MODPATH="${MODDIR}/Wen_kong"
 GITEE="https://gitee.com/qiuleyo/wen_kong_app/tree/master"
 
 LOGURL="${GITEE}/update.log"
-MODURL="https://gitee.com/qiuleyo/wen_kong_app/master/raw/Wen_kong-v4.3.zip"
+MODURL="https://gitee.com/qiuleyo/wen_kong_app/raw/master/Wen_kong-v4.3.zip"
 MODMD5="83f0b82a652fcd565fdc21e32696f07f"
 MODVERSION="v4.3"
 function download(){
