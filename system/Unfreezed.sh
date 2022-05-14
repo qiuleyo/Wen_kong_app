c047daa8c679b9872897e99feca0c3ce2f4ef7dd
qiule <3042627767@qq.com>
2022-05-14 05:48:28 +0000

    删除文件 system

--- system/Unfreezed.sh
+++
@@ -1,4 +0,0 @@
-pm enable com.xiaomi.joyose
-pm enable com.miui.powerkeeper
-pm enable com.xiaomi.powerchecker
-echo "- 已解冻"
--- system/freeze.sh
+++
@@ -1,4 +0,0 @@
-pm disable com.xiaomi.joyose
-pm disable com.miui.powerkeeper
-pm disable com.xiaomi.powerchecker
-echo "- 已冻结温控三件套"
