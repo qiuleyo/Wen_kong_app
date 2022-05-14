1471acd6817f21efc77214df3be343b0c4fe91fb
qiule <3042627767@qq.com>
2022-05-14 06:31:06 +0000

    ,

--- Wen_kong.sh
+++ Wen_kong.sh
@@ -144,10 +144,10 @@ if [[ ${PACKAGE_NAME} != com.fuckwenkong ]]; then
 		</text>
 	EOF
 else
-	if [[ ${PACKAGE_VERSION_CODE} != 1 ]]; then
+	if [[ ${PACKAGE_VERSION_CODE} != 11 ]]; then
 		cat <<-EOF
 			<text>
-				<slice u="true" align="center" break="true" link="" size="20">管理器不是最新版&#x000A;点击获取最新管理器下载链接</slice>
+				<slice u="true" align="center" break="true" link="" size="20">管理器不是最新版&#x000A;点击获取最新APP下载链接</slice>
 			</text>
 		EOF
 	else
